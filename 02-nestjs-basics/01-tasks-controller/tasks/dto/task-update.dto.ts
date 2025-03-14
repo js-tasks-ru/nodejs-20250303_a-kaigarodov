486b657274f57b8863544095d015d117f575aced
kaigarodov <kaigarodov2012@yandex.ru>
2025-03-14 21:18:24 +0500

    fix imports

--- 02-nestjs-basics/01-tasks-controller/tasks/dto/task-update.dto.ts
+++ 02-nestjs-basics/01-tasks-controller/tasks/dto/task-update.dto.ts
@@ -1,6 +1,4 @@
 import { PartialType } from "@nestjs/mapped-types";
-import { IsIn, IsNotEmpty, IsOptional, IsString } from "class-validator";
-import { TaskStatus } from "tasks/task.model";
 import { TaskCreateDto } from "./task-create.dto";
 
 export class TaskUpdateDto extends PartialType(TaskCreateDto) {}
