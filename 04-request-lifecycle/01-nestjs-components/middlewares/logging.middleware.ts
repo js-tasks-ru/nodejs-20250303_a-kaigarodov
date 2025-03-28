4bbe222ffc9a101eb33743aa0fa2f05261699168
kaigarodov <kaigarodov2012@yandex.ru>
2025-03-28 14:43:11 +0500

    add solution for 04-nestjs-components middleware

--- 04-request-lifecycle/01-nestjs-components/middlewares/logging.middleware.ts
+++ 04-request-lifecycle/01-nestjs-components/middlewares/logging.middleware.ts
@@ -1,6 +1,15 @@
-import { NestMiddleware } from "@nestjs/common";
+import { Logger, NestMiddleware } from "@nestjs/common";
 import { Request, Response, NextFunction } from "express";
 
 export class LoggingMiddleware implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {}
+  private readonly logger = new Logger();
+
+  use(req: Request, _: Response, next: NextFunction) {
+    const { method, originalUrl, hostname, protocol } = req;
+    const message = `Request ${protocol}://${hostname}${originalUrl} ${method}`;
+
+    this.logger.log(message);
+
+    next();
+  }
 }
