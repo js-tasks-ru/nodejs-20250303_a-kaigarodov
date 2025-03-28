7d7187e8fdb78979ec9c1d323f350751f12735d7
kaigarodov <kaigarodov2012@yandex.ru>
2025-03-28 19:54:46 +0500

    04-nestjs-components add roles guard

--- 04-request-lifecycle/01-nestjs-components/guards/roles.guard.ts
+++ 04-request-lifecycle/01-nestjs-components/guards/roles.guard.ts
@@ -1,5 +1,18 @@
-import { CanActivate, ExecutionContext } from "@nestjs/common";
+import {
+  CanActivate,
+  ExecutionContext,
+  ForbiddenException,
+} from "@nestjs/common";
 
 export class RolesGuard implements CanActivate {
-  canActivate(context: ExecutionContext) {}
+  canActivate(context: ExecutionContext) {
+    const request = context.switchToHttp().getRequest<Request>();
+    const role = request.headers["x-role"];
+
+    if (role !== "admin") {
+      throw new ForbiddenException();
+    }
+
+    return true;
+  }
 }
