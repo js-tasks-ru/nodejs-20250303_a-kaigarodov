76213cfa58e4a62559c4589a33642cc068e5b1ad
kaigarodov <kaigarodov2012@yandex.ru>
2025-03-29 14:15:01 +0500

    04-nestjs-components add filter

--- 04-request-lifecycle/01-nestjs-components/app.module.ts
+++ 04-request-lifecycle/01-nestjs-components/app.module.ts
@@ -1,9 +1,17 @@
 import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
 import { TasksModule } from "./tasks/tasks.module";
 import { LoggingMiddleware } from "./middlewares/logging.middleware";
+import { APP_FILTER } from "@nestjs/core";
+import { HttpErrorFilter } from "./filters/http-error.filter";
 
 @Module({
   imports: [TasksModule],
+  providers: [
+    {
+      provide: APP_FILTER,
+      useClass: HttpErrorFilter,
+    },
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
--- 04-request-lifecycle/01-nestjs-components/filters/http-error.filter.ts
+++ 04-request-lifecycle/01-nestjs-components/filters/http-error.filter.ts
@@ -1,5 +1,37 @@
-import { ArgumentsHost, ExceptionFilter } from "@nestjs/common";
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+  HttpStatus,
+} from "@nestjs/common";
 
+import { Response } from "express";
+
+import * as fs from "fs";
+
+@Catch()
 export class HttpErrorFilter implements ExceptionFilter {
-  catch(exception: any, host: ArgumentsHost) {}
+  catch(exception: Error, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    const timestamp = new Date().toISOString();
+    const { message, stack } = exception;
+    const status =
+      exception instanceof HttpException
+        ? exception.getStatus()
+        : HttpStatus.INTERNAL_SERVER_ERROR;
+
+    fs.appendFileSync(
+      "errors.log",
+      `[${timestamp}] ${status} - ${message}\nStack: ${stack}\n`,
+    );
+
+    response.status(status).json({
+      statusCode: status,
+      timestamp,
+      message,
+    });
+  }
 }
