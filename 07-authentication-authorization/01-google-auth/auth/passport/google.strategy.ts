701c19e8b26628e1710c9d76e88a0cc85464a54d
kaigarodov <kaigarodov2012@yandex.ru>
2025-04-30 22:54:00 +0500

    07-google-auth solution

--- 07-authentication-authorization/01-google-auth/auth/passport/google.strategy.ts
+++ 07-authentication-authorization/01-google-auth/auth/passport/google.strategy.ts
@@ -1,8 +1,37 @@
 import { PassportStrategy } from "@nestjs/passport";
 import { Profile, Strategy } from "passport-google-oauth20";
-import { Injectable } from "@nestjs/common";
+import { Inject, Injectable } from "@nestjs/common";
+import authConfig from "../../configs/auth.config";
+import { ConfigType } from "@nestjs/config";
+import { UsersService } from "../../users/users.service";
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy) {
-  async validate(accessToken: string, refreshToken: string, profile: Profile) {}
+  constructor(
+    @Inject(authConfig.KEY)
+    config: ConfigType<typeof authConfig>,
+    private usersService: UsersService,
+  ) {
+    super({
+      clientID: config.GOOGLE_CLIENT_ID,
+      clientSecret: config.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/callback",
+      scope: ["profile"],
+    });
+  }
+
+  async validate(accessToken: string, refreshToken: string, profile: Profile) {
+    const { id, displayName, photos } = profile;
+
+    const user = await this.usersService.findOne(id);
+
+    if (user) return user;
+
+    return this.usersService.create({
+      id,
+      displayName,
+      avatar: photos[0]?.value,
+      password: "",
+    });
+  }
 }
