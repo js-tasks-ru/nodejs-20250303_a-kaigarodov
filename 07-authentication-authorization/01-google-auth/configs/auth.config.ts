b52a010fafc51c6964d56d01d4951405771ff8d3
kaigarodov <kaigarodov2012@yandex.ru>
2025-05-01 00:09:13 +0500

    fix

--- 07-authentication-authorization/01-google-auth/configs/auth.config.ts
+++ 07-authentication-authorization/01-google-auth/configs/auth.config.ts
@@ -2,9 +2,10 @@ import { registerAs } from "@nestjs/config";
 
 export default registerAs("auth", () => {
   return {
-    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || "",
-    GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET || "",
-    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY || "",
-    JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || "",
+    GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID || "GOOGLE_CLIENT_ID",
+    GOOGLE_CLIENT_SECRET:
+      process.env.GOOGLE_CLIENT_SECRET || "GOOGLE_CLIENT_SECRET",
+    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY || "killer-is-jim",
+    JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || "1h",
   };
 });
